18602879651430b99dad21fda9d64d8672cbdd46
Darshan Bhokare <56796768+DarshanBhokare@users.noreply.github.com>
2023-08-18 02:58:15 -0400

    Time: 208 ms (13.17%), Space: 90.6 MB (5.02%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,11 +1,12 @@
 class Solution {
     
     Map<Integer, List<Integer>> graph = new HashMap<>();
-    boolean[] visited;
+    //boolean[] visited;
+    Set<Integer> visited= new HashSet<>();
     Set<String> roads = new HashSet<>();
     public int minReorder(int n, int[][] connections) {
         
-        visited = new boolean[n];
+        //visited = new boolean[n];
         
         for(int i=0;i<n;i++){
             graph.put(i, new ArrayList<>());
@@ -22,7 +23,8 @@ class Solution {
         }
         
         
-        visited[0] = true;
+        //visited[0] = true;
+        visited.add(0);
         
         return dfs(0);
         
@@ -32,11 +34,12 @@ class Solution {
     public int dfs(int city){
         int result=0;
         for(int neighbor: graph.get(city)){
-            if(!visited[neighbor]){
+            if(!visited.contains(neighbor)){
                 if(roads.contains(String.valueOf(city)+","+String.valueOf(neighbor))){
                     result++;
                 }
-                visited[neighbor]=true;
+                //visited[neighbor]=true;
+                visited.add(neighbor);
                 result += dfs(neighbor);
             }
         }
