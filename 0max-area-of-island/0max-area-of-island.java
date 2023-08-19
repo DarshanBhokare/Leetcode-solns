3a27335ff562908f7200e673b510d42090063c9e
Darshan Bhokare <56796768+DarshanBhokare@users.noreply.github.com>
2023-08-19 15:39:54 -0400

    Time: 3 ms, Memory: 43.3 MB - LeetHub

---
+++ 0max-area-of-island/0max-area-of-island.java
@@ -0,0 +1,45 @@
+class Solution {
+    
+    boolean[][] visited;
+    int[][] directions=new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+    int maxArea, m, n,area;
+    public int maxAreaOfIsland(int[][] grid){ 
+        maxArea=0;
+        m = grid.length;
+        n = grid[0].length;
+        
+        visited = new boolean[m][n];
+        
+        for(int i = 0;i<m;i++)
+        {
+            for(int j=0;j<n;j++){
+                if(!visited[i][j] && grid[i][j]==1){
+                    visited[i][j]=true;
+                    area=1;
+                    dfs(i,j,grid);
+                    maxArea=Math.max(maxArea, area);
+                }
+            }
+        }        
+        
+        return maxArea;
+    }
+    
+    private boolean isValid(int row, int col, int[][] grid){
+        return (row>=0 && row<m) && (col>=0 && col<n) && (grid[row][col]==1);
+        
+    }
+    private void dfs(int row, int col, int[][] grid){
+        for(int[] direction: directions){
+            int rowNew = row + direction[0], colNew = col + direction[1];
+            if(isValid(rowNew, colNew, grid) && !visited[rowNew][colNew]){
+                visited[rowNew][colNew]=true;
+                area+=1;
+                
+                dfs(rowNew, colNew, grid);
+                
+            }
+      
+        }
+    }
+}
