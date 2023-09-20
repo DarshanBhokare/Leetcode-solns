42c77772f35ae6567be63ddda98b26ed99cf3a65
Darshan Bhokare <56796768+DarshanBhokare@users.noreply.github.com>
2023-09-20 16:27:58 -0400

    Time: 2 ms (99.48%), Space: 42.1 MB (66.73%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,23 +1,34 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // Convert the input string to lowercase
+        /// Convert the input string to lowercase
         s = s.toLowerCase();
         
-        // Remove all non-alphanumeric characters using regex
-        s = s.replaceAll("[^a-zA-Z0-9]", "");
-        
-        // Check if the resulting string is a palindrome
         int left = 0;
         int right = s.length() - 1;
         
         while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
+            char leftChar = s.charAt(left);
+            char rightChar = s.charAt(right);
+            
+            // Skip non-alphanumeric characters
+            if (!isAlphanumeric(leftChar)) {
+                left++;
+            } else if (!isAlphanumeric(rightChar)) {
+                right--;
+            } else {
+                // Compare alphanumeric characters
+                if (leftChar != rightChar) {
+                    return false;
+                }
+                left++;
+                right--;
             }
-            left++;
-            right--;
         }
         
         return true;
+    
+    }
+    private boolean isAlphanumeric(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
     }
 }
