5dc7e53a4afc008af310583e82966a1219dbba24
Darshan Bhokare <56796768+DarshanBhokare@users.noreply.github.com>
2023-09-20 23:36:45 -0400

    Time: 4 ms (99.39%), Space: 46.9 MB (15.79%) - LeetHub

---
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+         if (k <= 1) {
+            return 0;
+        }
+        int numberOfSubArrays=0;
+        int curr=1;
+        int left = 0;
+        for(int right =0;right<nums.length;right++){
+            curr*=nums[right];
+            
+            while(curr>=k){
+                curr=curr/nums[left];
+                left++;
+            }
+            numberOfSubArrays+=right-left+1;
+           
+        }
+        return numberOfSubArrays;
+        
+        
+        
+        
+    }
+}
