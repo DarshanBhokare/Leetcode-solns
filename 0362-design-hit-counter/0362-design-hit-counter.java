63e1354a6e38c52bcab4c1a043470882b49aadb8
Darshan Bhokare <56796768+DarshanBhokare@users.noreply.github.com>
2024-08-22 21:04:23 -0400

    Time: 2 ms (11.73%), Space: 41.9 MB (10.55%) - LeetHub

--- 0362-design-hit-counter/0362-design-hit-counter.java
+++ 0362-design-hit-counter/0362-design-hit-counter.java
@@ -10,17 +10,13 @@ class HitCounter {
     }
     
     public void hit(int timestamp) {
-        System.out.println("Hit at" + timestamp);
-        // timestampHitsMap.put(timestamp,++lifeTimeHits - timestampHitsMap.getOrDefault(timestamp-lookback,0));
+
         timestampHitsMap.put(timestamp,++lifeTimeHits);
-        System.out.println("Hit map after hit " + timestampHitsMap);
-        System.out.println("All time hits "+ lifeTimeHits);
         hitsOrder.push(timestamp);
     }
     
     public int getHits(int timestamp) {
         int startTimestamp=timestamp;
-        System.out.println("Returning hits at " + timestamp);
         while(!timestampHitsMap.containsKey(timestamp)){
             
             timestamp--;
@@ -28,27 +24,18 @@ class HitCounter {
                 return 0;
             }
         }
-        System.out.println("Updaated timestamp is " + timestamp);
 
         if(!timestampHitsMap.containsKey(startTimestamp-lookback) && startTimestamp-lookback > 0){
             timestampHitsMap.put(startTimestamp-lookback, lastKnownHit(startTimestamp-lookback));
         }
-        System.out.println("Hits at last know timestmap for lookback" + timestampHitsMap.get(startTimestamp-lookback));
-                System.out.println("Finally Returning hit for " + timestamp + "From " + timestampHitsMap);
+        
         return timestampHitsMap.get(timestamp) - timestampHitsMap.getOrDefault(startTimestamp-lookback,0);
     }
     
     private int lastKnownHit(int timestamp){
-            
-            // while(!timestampHitsMap.containsKey(timestamp)){
-            //     timestamp--;
-            //     if(timestamp==0){
-            //         break;
-            //     }
-            // }
         
         Stack<Integer> tempStack = new Stack<>();
-        System.out.println("stack while calculating last known" + hitsOrder);
+
         while(timestamp<=hitsOrder.peek()){
             int num = hitsOrder.pop();
             tempStack.push(num);
@@ -57,7 +44,6 @@ class HitCounter {
                 break;
                     
             }
-
         }
         
         timestamp = hitsOrder.peek();
@@ -65,7 +51,6 @@ class HitCounter {
         while(!tempStack.isEmpty()){
             hitsOrder.push(tempStack.pop());
         }
-System.out.println("stack after calculating last known" + hitsOrder);
         return timestampHitsMap.getOrDefault(timestamp,0);
     }
 }
