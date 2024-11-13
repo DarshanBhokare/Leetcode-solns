32948fadf9a3a888ee6dae2f157c500b7ed44ae0
Darshan Bhokare <56796768+DarshanBhokare@users.noreply.github.com>
2024-11-13 08:05:41 +0530

    Time: 61 ms, Memory: 44.9 MB - LeetHub

---
+++ minimum-cost-to-connect-sticks/minimum-cost-to-connect-sticks.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int connectSticks(int[] sticks) {
+         PriorityQueue<Integer> orderedList = new PriorityQueue<>();
+        
+        int totalCost = 0;
+        
+        for(int length: sticks){
+            orderedList.add(length);
+        }
+        
+        while(orderedList.size() > 1){
+            int firstStick = orderedList.remove();
+            int secondStick = orderedList.remove();
+            int newStick = firstStick + secondStick;
+            orderedList.add(newStick);
+            totalCost += newStick;
+        }
+        
+        
+        
+        return totalCost;
+        
+    }
+}
